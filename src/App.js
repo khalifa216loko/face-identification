43a3cbc03889f58bc1bcfd73c707aa98e41a9d90
Ben <akhlifa@students.beaminstitute.org>
2024-10-01 14:49:16 +0200

    Initial commit of React face personalization app

--- src/App.js
+++ src/App.js
@@ -1,102 +1,89 @@
-import React, { useEffect, useState, useRef } from 'react';
-import * as faceapi from 'face-api.js';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import * as faceapi from "face-api.js";
 
-function App() {
-  const videoHeight = 720;
-  const videoWidth = 550;
-  const videoRef = useRef();
-  const canvasRef = useRef();
-  const [detect, setDetect] = useState(false);
+const App = () => {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
 
+  // Load face-api models and start the webcam
   useEffect(() => {
     const loadModels = async () => {
-      const MODEL_URL = process.env.PUBLIC_URL + '/models';
-      Promise.all([
-        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
-        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
-        faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL) //heavier/accurate version of tiny face detector
-      ]).then(startVideo);
-    }
+      await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+      await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
+      await faceapi.nets.faceExpressionNet.loadFromUri("/models");
+      await faceapi.nets.ageGenderNet.loadFromUri("/models");
+      startVideo();
+    };
+
+    // Start video stream from the webcam
+    const startVideo = () => {
+      navigator.mediaDevices.getUserMedia({ video: {} })
+        .then((stream) => {
+          videoRef.current.srcObject = stream;
+        })
+        .catch((err) => console.error("Error accessing webcam:", err));
+
+      videoRef.current.addEventListener('play', () => {
+        // Perform face detection when the video starts playing
+        setInterval(async () => {
+          const detections = await faceapi.detectAllFaces(
+            videoRef.current,
+            new faceapi.TinyFaceDetectorOptions()
+          )
+            .withFaceLandmarks()
+            .withFaceExpressions()
+            .withAgeAndGender();
+
+          const canvas = canvasRef.current;
+          const ctx = canvas.getContext("2d");
+          const { videoWidth, videoHeight } = videoRef.current;
+          
+          canvas.width = videoWidth;
+          canvas.height = videoHeight;
+          ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+          const resizedDetections = faceapi.resizeResults(detections, {
+            width: videoWidth,
+            height: videoHeight,
+          });
+          faceapi.draw.drawDetections(canvas, resizedDetections);
+          faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+
+          // Display gender, age, and emotion
+          resizedDetections.forEach((result) => {
+            const { gender, age, expressions } = result;
+            const { x, y, width, height } = result.detection.box;
+            ctx.font = "16px Arial";
+            ctx.fillStyle = "blue";
+
+            // Display gender and age
+            ctx.fillText(`${gender} - ${Math.round(age)} years`, x, y - 10);
+
+            // Display emotion
+            const mostLikelyEmotion = Object.keys(expressions).reduce((a, b) =>
+              expressions[a] > expressions[b] ? a : b
+            );
+            ctx.fillText(`Emotion: ${mostLikelyEmotion}`, x, y + height + 20);
+          });
+        }, 100);
+      });
+    };
 
     loadModels();
   }, []);
 
-  const startVideo = () => {
-    navigator.getUserMedia(
-      {video: {}},
-      stream => videoRef.current.srcObject = stream,
-      err => console.error(err)
-    );
-  }
-
-  const loadLabeledImages = () => {
-    const LABELS_URL = process.env.PUBLIC_URL + '/labeled_images/';
-    const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark', 'Umair Nadeem'];
-    // const labels = ['Umair Nadeem'];
-
-    return Promise.all(
-        labels.map(async (label) => {
-            const descriptions = [];
-            for(let i=1 ; i<=2 ; i++) {
-                // const img = await faceapi.fetchImage(`https://eximo-hrms.s3.amazonaws.com/labeled_images/${label}/${i}.jpg`);
-                const img = await faceapi.fetchImage(`${LABELS_URL}${label}/${i}.jpg`);
-                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
-
-                descriptions.push(detections.descriptor);
-            }
-            document.body.append(label + 'Faces Loaded');
-            return new faceapi.LabeledFaceDescriptors(label, descriptions);
-        })
-    )
-  }
-
-  const handleVideoOnPlay = async () => {
-    const labeledDescriptors = await loadLabeledImages();
-    const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.6);
-
-    // videoRef.current.addEventListener('play', () => {
-        console.log('video playing...');
-
-        canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(videoRef.current);
-        // document.body.append(canvas);
-
-        const displaySize = { width: videoWidth, height: videoHeight };
-        faceapi.matchDimensions(canvasRef.current, displaySize);
-
-        setInterval(async () => {
-            const detections = await faceapi.detectAllFaces(videoRef.current).withFaceLandmarks().withFaceDescriptors();
-
-            const resizedDetections = faceapi.resizeResults(detections, displaySize);
-            canvasRef.current.getContext('2d').clearRect(0, 0, videoWidth, videoHeight);
-
-            const results = resizedDetections.map((d) => {
-                return faceMatcher.findBestMatch(d.descriptor);
-            })
-
-            results.forEach((result, i) => {
-                const box = resizedDetections[i].detection.box;
-                const drawBox = new faceapi.draw.DrawBox(box, {label: result.toString()});
-
-                drawBox.draw(canvasRef.current);
-
-                if (result._label === 'Umair Nadeem') {
-                  setDetect(true);
-                }
-            });
-        }, 100);
-    // })
-  }
-
   return (
-    <div className="App">
-      <span>{detect ? 'Detected' : 'Not Detected yet...'}</span>
-      <div className="display-flex justify-content-center">
-        <video ref={videoRef} id="videoInput" width={videoWidth} height={videoHeight} muted controls onPlay={handleVideoOnPlay}></video>
-        <canvas ref={canvasRef} />
-      </div>
+    <div>
+      <video
+        ref={videoRef}
+        autoPlay
+        muted
+        style={{ display: "block", width: "100%" }}
+      />
+      <canvas ref={canvasRef} style={{ position: "absolute", top: 0, left: 0 }} />
     </div>
   );
-}
+};
 
 export default App;
+
